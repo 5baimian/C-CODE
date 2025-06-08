1070713ae3a2e06580a139dc87b6ba8842fbdb08
5baimian <24163339565@qq.com>
2025-06-08 15:26:10 +0800

    更改题目名称并增加readme文件

--- day02/day02/day02_01.c
+++ day02/day02/day02_01.c
@@ -1,5 +1,4 @@
 
-
 /*B01
 求Sn=2+22+222+…+22…222（有n个2）之值。
 例如：2+22+222+2222+22222（n=5），n由键盘输入。(n<=6)*/
@@ -8,7 +7,7 @@
 //#include <math.h>
 //int main()
 //{
-//	int i = 0, n = 0, Sn = 0,Sn1=0;
+//	int i = 0, n = 0, Sn = 0, Sn1 = 0;
 //restart:
 //	printf("请输入n的值：n= ");
 //	scanf_s("%d", &n);
@@ -24,6 +23,6 @@
 //		goto restart;
 //
 //
-//	printf("Sn1=%d\n",Sn1);
+//	printf("Sn1=%d\n", Sn1);
 //	return 0;
 //}
