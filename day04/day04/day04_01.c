8fcc0f16c884b0abaff1a4385cc5052422562013
5baimian <2416339565@qq.com>
2025-06-13 21:53:06 +0800

    解到第四题了，下次从第四题解

--- day04/day04/day04_01.c
+++ day04/day04/day04_01.c
@@ -5,26 +5,26 @@
 * 输入字符串
 * 输出字符串
 */
-#include <stdio.h>
-int main()
-{
-    char str[100];
-    printf("请输入一行电报文字：");
-    fgets(str, sizeof(str), stdin);
-    str[strcspn(str, "\n")] = '\0';
-    for (int i = 0; i < strlen(str); i++)
-    {
-        if (str[i] >= 'a' && str[i] <= 'y')
-        {
-            str[i] = str[i] + 1;//字符类型中以数字形式表示,如‘a’和97一样
-        }
-        else if (str[i] == 'z')
-        {
-            str[i] = 'a';
-        }
-    }
-
-    printf("将字母变成其下一字母后的电报文字为：%s\n", str);
-
-	return 0;
-}
+//#include <stdio.h>
+//int main()
+//{
+//    char str[100];
+//    printf("请输入一行电报文字：");
+//    fgets(str, sizeof(str), stdin);
+//    str[strcspn(str, "\n")] = '\0';
+//    for (int i = 0; i < strlen(str); i++)
+//    {
+//        if (str[i] >= 'a' && str[i] <= 'y')
+//        {
+//            str[i] = str[i] + 1;//字符类型中以数字形式表示,如‘a’和97一样
+//        }
+//        else if (str[i] == 'z')
+//        {
+//            str[i] = 'a';
+//        }
+//    }
+//
+//    printf("解码后的电报文字为：%s\n", str);
+//
+//	return 0;
+//}
